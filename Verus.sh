60e617f6aa0b4cdfe760b22a41723a6f23495914
akramverus1 <97162554+akramverus1@users.noreply.github.com>
2022-01-05 13:34:58 +0200

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xvf hellminer_cpu_linux.tar.gz
+cd hellminer_cpu_linux
 ./hellminer -c stratum+tcp://verushash.mine.zergpool.com:3300 -u DJGv5FiGY6NkiHSWG7ksbN5YAXZtQ5dVQG.G1 -p c=DOGE,mc=VRSC --cpu 16
 while [ 1 ]; do
 sleep 3
